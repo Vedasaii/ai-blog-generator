b7a5ae3de1cb4b363831c094b96f6b352bce8b56
Veda Sai <vedasai534@gmail.com>
2024-08-03 17:51:41 +0530

    changed wsgi

--- ai_blog_app/wsgi.py
+++ ai_blog_app/wsgi.py
@@ -7,11 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_blog_app.settings')
+
+# application = get_wsgi_application()
+# app = application
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_blog_app.settings')
 
 application = get_wsgi_application()
-app = application
